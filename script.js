f352f64a8a1aa88fea010fd9f6ca5e1aaa128674
Tushar Ahuja <93457512+Tusharahuja14@users.noreply.github.com>
2025-01-07 00:25:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 function makeid(l) {
   // write your code here
+	let string='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+	let result='';
+	for(let i=0;i<l;i++)
+		{
+			let ch=Math.floor(Math.random()*string.length);
+			result+=string.charAt(ch);
+		}
+	return result;
 }
 
 // Do not change the code below.
+/*
 const l = prompt("Enter a number.");
 alert(makeid(l));
